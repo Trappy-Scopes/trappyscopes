e35600299a82b53839972de666a54a6386fe3155
yatharthb97 <yatharth1997@gmail.com>
2024-01-25 17:03:55 +0000

    Fixed indentation on install_libs.py script

--- install_py_libs.py
+++ install_py_libs.py
@@ -2,11 +2,11 @@ import pip
 
 def install_py_libs(required_libs):
 	for lib in required_libs:
-    try: 
-       __import__(lib)
-         print(lib, "exists!")
-    except ImportError:
-      pip.main(['install', lib])
+		try: 
+			__import__(lib)
+			print(lib, "exists!")
+		except ImportError:
+			pip.main(['install', lib])
 			print(lib, "was installed!")
 
 
