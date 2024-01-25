ba39c3e12f75ebd31635100095ef5c8a2c9833f4
yatharthb97 <yatharth1997@gmail.com>
2024-01-25 20:33:41 +0000

    modified script

--- scripts/framerate_consistency_analysis.py
+++ scripts/framerate_consistency_analysis.py
@@ -9,13 +9,13 @@ exp = Experiment(f"{scopeid}_framerate_consistency_{dt}")
 sleep(2)
 
 
-lit.setVs(1,1,1)
+lit.setVs(1,0,0)
 
 ## Capture for an hour
 for hour in [1, 4, 8, 16]:
 	name = f"{hour}_hour"
 	#cam.en_pre_timestamps(f"{name}_pre_callback_ts.txt")
-	capture(vidmp4, f"{name}.mp4", tsec=60*60, it=hour, it_delay_s=30)
+	capture(vidmp4, f"{name}.mp4", tsec=60, it=hour, it_delay_s=30)
 
 
 # Close experiment
