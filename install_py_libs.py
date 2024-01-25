6e481be1aa380ce830deec20aca7e3f6c3957b4a
yatharthb97 <yatharth1997@gmail.com>
2024-01-25 17:02:27 +0000

    Added install_libs.py script

---
+++ install_py_libs.py
@@ -0,0 +1,16 @@
+import pip
+
+def install_py_libs(required_libs):
+	for lib in required_libs:
+    try: 
+       __import__(lib)
+         print(lib, "exists!")
+    except ImportError:
+      pip.main(['install', lib])
+			print(lib, "was installed!")
+
+
+if __name__ == "__main__":
+	reqd_libs = ["pyyaml", "colorama", "nanoid"]
+
+	install_py_libs(reqd_libs)
