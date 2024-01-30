5a276d4f1a1a374a08a59e470e31abc4e543d9d9
yatharthb97 <yatharth1997@gmail.com>
2024-01-30 13:58:01 +0000

    Script changes - test

--- scripts/framerate_consistency_analysis.py
+++ scripts/framerate_consistency_analysis.py
@@ -12,7 +12,7 @@ sleep(2)
 lit.setVs(0,0.5,0)
 
 ## Capture for an hour
-for hour in [4, 8, 16]:
+for hour in [1, 4, 8, 16]:
 	name = f"{hour}_hour"
 	cam.en_pre_timestamps(f"{name}_pre_callback_ts.txt")
 	#print(f"FPS: {cam.cam.video_configuration.controls.FrameRate}")
