61e35d4b090384d2eadb353c3ded46419f45646d
yatharthb97 <yatharth1997@gmail.com>
2024-02-06 17:56:05 +0000

    Updated experimental conditions

--- scripts/motor_calibration_test.py
+++ scripts/motor_calibration_test.py
@@ -21,7 +21,7 @@ sleep(0)
 ## Go through different speeds
 exp.logs["speed_test"] = {}
 #speedset = [0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.5, 0.75, 0.8, 0.95]
-speedset = [1.0, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, .0]
+speedset = [0.99, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, .0]
 print(f"Speeds: {speedset}")
 
 for i, speed in enumerate(speedset):
