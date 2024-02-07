4501f2f6f50881b166396759050ea3ac312f7eb4
yatharthb97 <yatharth1997@gmail.com>
2024-02-07 12:01:15 +0000

    Fixed exp motor.duty bug

--- scripts/motor_calibration_test.py
+++ scripts/motor_calibration_test.py
@@ -21,8 +21,13 @@ sleep(0)
 ## Go through different speeds
 exp.logs["speed_test"] = {}
 #speedset = [0.05, 0.10, 0.15, 0.2, 0.25, 0.3, 0.5, 0.75, 0.8, 0.95]
-speedset = [0.99, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+#speedset = [0.99, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
+
+## extensive
+speedset = [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.15, 0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.75, 0.8, 0.95, 0.99]
+
 print(f"Speeds: {speedset}")
+print(f"Will test {len(speedset)} sppeds!")
 
 for i, speed in enumerate(speedset):
 	
