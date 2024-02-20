36d1d83eb584ed23aa97c8495bcab66a1f21c9c0
yatharthb97 <yatharth1997@gmail.com>
2024-02-20 12:23:31 +0000

    scripts/luxcalibration tryouts

--- scripts/luxcalibration.py
+++ scripts/luxcalibration.py
@@ -6,7 +6,8 @@ import time
 
 # Start experiment
 dt = str(datetime.date.today()).replace("-", "_")
-exp = Test(f"{scopeid}_test_cameraclosure_{dt}")
+t = time.localtime(time.time())
+exp = Calibration(f"{scopeid}_luxcalib_{dt}_{t.tm_hour}_{t.tm_min}_{t.tm_sec}")
 print(lit)
 colors = {}
 colors["r"] = 0.0
@@ -20,6 +21,6 @@ for ch in ["r", "g", "b"]:
 	for i in range(levels):
 		colors[ch] = colors[ch] + ((3.3/levels)*i)
 		pico(f'l1.setVs({colors["r"]}, {colors["g"]}, {colors["b"]})')
-		capture(vidmp4, f"{ch}-{colors[ch]}.mp4", tsec=10, init_delay_s=5)
+		capture(vidmp4, f"{ch}-{colors[ch]}.mp4", tsec=10, init_delay_s=0)
 # Close experiment
 exp.close()
