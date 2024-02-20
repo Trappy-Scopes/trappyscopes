f603c5828a98d643499f8271ee22e977113d5e7b
yatharthb97 <yatharth1997@gmail.com>
2024-02-20 13:40:32 +0000

    Modified motor calibration test

--- scripts/motor_calibration_test.py
+++ scripts/motor_calibration_test.py
@@ -26,11 +26,23 @@ exp.logs["speed_test"] = {}
 #speedset = [0.99, 0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09]
 
 ## extensive
+fill_mL = 20
+freq = 10
 speedset = [0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.15, \
 			0.2, 0.25, 0.3, 0.4, 0.5, 0.6, 0.75, 0.8, 0.95, 0.99]
 
+
+## Set Frequency
+pico(f"motor.fwdpin.freq({freq})")
+pico(f"motor.revpin.freq({freq})")
+print(f"Fwd pin freq: {pico('print(motor.fwdpin.freq())')}")
+
 print(f"Speeds: {speedset}")
 print(f"Will test {len(speedset)} sppeds!")
+
+
+## Plotting
+plotdata = {"speed": [], "duration": []}
 
 for i, speed in enumerate(speedset):
 	
@@ -41,15 +53,29 @@ for i, speed in enumerate(speedset):
 	pico(f"motor.speed({speed})")
 	print("Speed updated.")
 	
-	exp.user_prompt(None, label="stop")  ## Press enter to stop
+	inp = exp.user_prompt(None, label="stop")  ## Press enter to stop
 	pico("motor.hold()")
 	stop = time.perf_counter()
 
 	dur = stop-start
-	result = {"duty":float(pico("print(motor.duty)").rstrip("\r\n")), "speed": speed, "freq":10,  "duration":dur, "mL":20	, "setup": "syringe", "overflow": None}
+	result = {"duty":(pico("print(motor.duty)").rstrip("\r\n")), 
+				  "speed": speed, "freq":freq,  "duration":dur, "mL":fill_mL,
+				  "setup": "ibidi_0pt2mm_syringe_to_syringe", "overflow": 0, "success": (str(inp).strip() != "fail"),
+				  "experiment_type": "pwm_freq_perturbation"}
 	pprint.pprint(result)
 
 	exp.logs["speed_test"][i] = result
 
+	plotdata["speed"].append(speed)
+	plotdata["duration"].append(dur)
+
+
+	## Plot duration vs speed
+	plt.clf()
+	plt.xlabel(f"duration for filling {fill_mL}mL (seconds)")
+	plt.ylabel("normalised speed")
+
+	plt.plot(plotdata["duration"], plotdata["speed"], label=f"{freq}Hz")
+	plt.show()
 pico("motor.hold()")
 exp.close()
