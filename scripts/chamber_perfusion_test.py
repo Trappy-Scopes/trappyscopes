78a45615f426502be9c699a3d5d54229601e5d1a
yatharthb97 <yatharth1997@gmail.com>
2024-02-26 11:58:54 +0000

    Corrected script

--- scripts/chamber_perfusion_test.py
+++ scripts/chamber_perfusion_test.py
@@ -34,8 +34,8 @@ speedset = [0.00, 0.01, 0.02, 0.04, 0.05, 0.06, 0.07, 0.08, 0.09, 0.10, 0.15, \
 
 
 ## Set Frequency
-pico(f"motor.fwdpin.freq({freq})")
-pico(f"motor.revpin.freq({freq})")
+motor_pico(f"motor.fwdpin.freq({freq})")
+motor_pico(f"motor.revpin.freq({freq})")
 print(f"Fwd pin freq: {motor_pico('print(motor.fwdpin.freq())')}")
 
 print(f"Speeds: {speedset}")
