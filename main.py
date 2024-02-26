74b64ffdf8cdb4877620b6d12f83efde8b3146e7
yatharthb97 <yatharth1997@gmail.com>
2024-02-26 11:49:25 +0000

    Corrected script

--- main.py
+++ main.py
@@ -82,8 +82,8 @@ picomode = "null" * (device_metadata["hardware"]["pico"][0] == "nullpico") + \
 
 pico = RPiPicoDevice.Select(picomode, name=device_metadata["hardware"]["pico"][1], \
 	   connect=False)
-#pico.auto_connect()
-pico.connect("/dev/ttyACM1")
+pico.auto_connect()
+#pico.connect("/dev/ttyACM1")
 print(pico)
 if not pico.connected:
 	log.error("Could not get a pico device - exiting.")
