81faeb2f4df7a68a948c55a90e753edb8b823685
yatharthb97 <yatharth1997@gmail.com>
2024-03-08 11:58:54 +0000

    Added fwd and rev testing

---
+++ scripts/pumptests/fwdrev.py
@@ -0,0 +1,35 @@
+import datetime
+from experiment import Test
+import numpy as np
+import time
+
+# Start experiment
+unique_check = False
+dt = str(datetime.date.today()).replace("-", "_")
+exp = Test(f"{scopeid}_fwd_rev_test_{dt}")
+test = exp
+
+
+### Optional ------------------------
+## Incase of additional pico board
+motor_pico = RPiPicoDevice(connect=False)
+#motor_pico = NullRPiPicoDevice(connect=False)
+motor_pico.auto_connect()
+#motor_pico.connect("/dev/ttyACM0")
+motor_pico.exec_main()
+print(motor_pico)
+### ---------------------------------
+
+##OR
+motor_pico = pico
+
+
+speed = 0.4
+## Test opening and closing of camera
+for i in range(15):
+	motor_pico(f"motor.fwd(speec={speed})")
+	sleep(1)
+	motor_pico(f"motor.rev(speec={speed})")
+	sleep(1)
+motor_pico("motor.hold()")
+test.close()
