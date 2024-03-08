7cb5fc44a4dd5b6d627a3a70c43ff4a6ac75356f
yatharthb97 <yatharth1997@gmail.com>
2024-03-08 12:06:31 +0000

    Added fwd and rev testing

--- scripts/pumptests/fwdrev.py
+++ scripts/pumptests/fwdrev.py
@@ -28,8 +28,12 @@ speed = 0.4
 ## Test opening and closing of camera
 for i in range(15):
 	motor_pico(f"motor.fwd(speed={speed})")
-	sleep(1)
+	sleep(2)
+	motor_pico("motor.hold()")
+	sleep(2)
 	motor_pico(f"motor.rev(speed={speed})")
-	sleep(1)
+	sleep(2)
+	motor_pico("motor.hold()")
+	sleep(2)
 motor_pico("motor.hold()")
 test.close()
