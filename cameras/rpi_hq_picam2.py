5a4da3dab5e65ba0c0ac3af65f4a15229a0b9494
yatharthb97 <yatharth1997@gmail.com>
2024-03-27 14:17:36 +0000

    Trying to fix blackscrren on preview

--- cameras/rpi_hq_picam2.py
+++ cameras/rpi_hq_picam2.py
@@ -278,8 +278,8 @@ class Camera(AbstractCamera):
 		"""
 		print(f"Preview: tsec: {tsec}")
 		self.cam.title_fields = self.win_title_fields
-		self.cam.start(show_preview=True)
-		#self.cam.start_preview(self.preview_type)
+		#self.cam.start(show_preview=True)
+		self.cam.start_preview(preview=self.preview_type)
 		sleep(tsec)
 		self.cam.stop()
 		self.cam.stop_preview()
