677c441919c3b37b4f0d9f35b4ba367fcb04c2fc
yatharthb97 <yatharth1997@gmail.com>
2024-05-17 15:48:53 +0100

    added redundancy for pico connection failure

--- main.py
+++ main.py
@@ -203,11 +203,12 @@ else:
 print(Markdown("# SCOPE READY"))
 scope = ScopeAssembly()
 scope.add_device("cam", cam, description="Main camera.")
-scope.add_device("pico", pico, description="Main microcontroller on Serial.")
-scope.add_device("picoprox", RPiPicoDevice.Emit("", pico))
-all_pico_devs = pico.exec_cleanup("print(Handshake.obj_list(globals_=globals()))")
-for d in all_pico_devs:
-	scope.add_device(d, RPiPicoDevice.Emit(d, pico), description="Pico peripheral.")
+if pico:
+	scope.add_device("pico", pico, description="Main microcontroller on Serial.")
+	scope.add_device("picoprox", RPiPicoDevice.Emit("", pico))
+	all_pico_devs = pico.exec_cleanup("print(Handshake.obj_list(globals_=globals()))")
+	for d in all_pico_devs:
+		scope.add_device(d, RPiPicoDevice.Emit(d, pico), description="Pico peripheral.")
 scope.draw_tree()
 
 # Defining variables for common modes
