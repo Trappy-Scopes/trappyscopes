78689af8e5ccff1275cb0fa579ebb72f4c714b27
yatharthb97 <yatharth1997@gmail.com>
2024-05-27 14:08:55 +0100

    updated script for still resolution tests

--- scripts/resolutiontests/still_res_test.py
+++ scripts/resolutiontests/still_res_test.py
@@ -6,112 +6,145 @@ import time
 
 from cleaners import safepicam2_config
 
+
+
+
+description=
+"""
+Protocol to measure resolution with several modes - 4 modes. 4 colors, 3 images per channel.
+
+Steps:
+	1. Focus and magnification - record a 2mm chamber. At fullframe.
+	2. Set configuration -> open-camera --> Open a measurement stream per sensor mode --> Record through all the color channels (X3-iterations)-> Close camera.
+	3. Save measurement and acqs (1 image every 5 seconds)
+Camera configuration steps followed:
+	close-->open-->configure-->start-->close
+"""
+
+
+
+
 # Start experiment
 unique_check = False
 dt = str(datetime.date.today()).replace("-", "_")
 t = time.localtime(time.time())
-exp = Experiment(f"{scopeid}_still_res_test_usaf_{dt}_{t.tm_hour}_{t.tm_min}_{t.tm_sec}")
+exp = Experiment(f"{scopeid}_still_res_test_usaf_{dt}_{t.tm_hour}_{t.tm_min}", append_eid=True)
 test = exp
 
-#cam.config_cammode2()
-voltage = 0.5
-set_green = lambda : pico(f"l1.setVs(0,{voltage},0)")
-set_red = lambda : pico(f"l1.setVs({voltage},0,0)")
-set_blue = lambda : pico(f"l1.setVs(0,0,{voltage})")
-set_white = lambda : pico(f"l1.setVs({voltage},{voltage},{voltage})")
+
+### Configure experiment
+exp.attribs.update({"setup" : ["still_resolution_tests", "usaf_test_target"],
+					"description" : description,
+					"voltage": 2, "itr": 3,  "channels": ["r", "g", "b", "w"], 
+				    "res_set":[[1332, 990], [2028, 1080], [2028,1520], [4056, 3040], [1920, 1080]],
+
+				    "magnification": 1.0,
+				    "itr" : 3,
+
+				    "exposure_time": 50000,
+				    "awb_enable" :  False,
+				    "ae_enable" :  False,
+				    "brightness" :  0.3,
+				    "analogue_gain": 3,
+				    "contrast" :  2.5,
+				    "colour_gains" :  (0.,0.),
+				    "sharpness" :  1.,
+				    "saturation" :  .5,
+
+				    "quality" : 95,
+				    "compress_level": 0
+
+				   })
 
 
+## Configure light
+lightmap = {"r": [exp.attribs["voltage"], 0, 0], 
+			"g": [0, exp.attribs["voltage"], 0],
+			"b": [0, 0, exp.attribs["voltage"]], 
+			"w": [exp.attribs["voltage"], exp.attribs["voltage"], exp.attribs["voltage"]]
+		   }
+
+
+
+
+#### Focusing and magnification recording
+exp.user_prompt(None, label="Record 2mm-inhouse trap for magnification calibration.")
+scope.lit.setVs(1,1,1)
+sleep(2)
+cam.cam.create_still_configuration({"size":exp.attribs["res_set"][3]})
+cam.cam.start_and_capture_file("magnification_std.png")
+magms = exp.new_measurementstream("Magnification")
+m = magms(acq="magnification_std.png", config_size=list(cam.cam.still_configuration.sensor["output_size"]),
+		  size=cam.cam.still_configuration.size)
+
+
+
+
+### Begin experiment ---------------------------------------------------------------------------------
 cam.close()  ## Close camera
+for res in exp.attribs["res_set"]:
 
+	## Set measurement stream ##########
+	ms = new_measurementstream(tuple(res), 
+		                       measurements=["usaftt_group", 
+		                       			     "usaftt_element", 
+		                       			     "min_res_um"]) ## Calculated afterwards	
+	tab = ms.tabulate("rV", "gV", "bV", "res", "config_size")
 
-res_set = [[1332, 990], [2028, 1080], [2028,1520], [4056, 3040], [1920, 1080]]
-exp.logs["results"] = []
-exp.logs["res_set"] = res_set
-
-
-## Test opening and closing of camera
-for res in res_set:
+	###################################
 
 	## Set Camera settings:
 	cam.close()
 	cam.open()
-	#cam.configure(res=res)
-
-	fps=20.
-	framelimit=int(1000000/fps)
-	res_x = 1920
-	res_y = 1080
-	exposure_time=50000
-	awb_enable = False
-	ae_enable = False
-	brightness = 0.3
-	analogue_gain=3
-	contrast = 2.5
-	colour_gains = (0.,0.)
-	sharpness = 1.
-	saturation = .5
-	
-
-	#define a dictionary with the controls to save in file
-	controls={"FrameRate":fps,
-	"ExposureTime":exposure_time,
-	"AwbEnable":awb_enable,
-	"AeEnable":ae_enable,
-	"AnalogueGain":analogue_gain,
-	"Brightness":brightness,
-	"Contrast":contrast,
-	"ColourGains":colour_gains,
-	"Sharpness":sharpness,
-	"Saturation":saturation}
 
 	# create video configuration
-	cam.cam.create_still_configuration({"size":res}, controls=controls)
+	cam.cam.create_still_configuration({"size":res})
+	cam.cam.still_configuration.controls.ExposureTime = exp.attribs["exposure_time"]
+	cam.cam.still_configuration.controls.AwbEnable    = exp.attribs["awb_enable"]
+	cam.cam.still_configuration.controls.AeEnable     = exp.attribs["ae_enable"]
+	cam.cam.still_configuration.controls.AnalogueGain = exp.attribs["analogue_gain"]
+	cam.cam.still_configuration.controls.Brightness   = exp.attribs["brightness"]
+	cam.cam.still_configuration.controls.Contrast     = exp.attribs["contrast"]
+	cam.cam.still_configuration.controls.ColourGains  = exp.attribs["colour_gains"]
+	cam.cam.still_configuration.controls.Sharpness    = exp.attribs["sharpness"]
+	cam.cam.still_configuration.controls.Saturation   = exp.attribs["saturation"]
+	cam.cam.still_configuration.size = (res[0], res[1])
 
-	#the video configuration controls need to be 
-	#set again - set only the ones in 'controls'
-	cam.cam.still_configuration.controls.FrameRate = fps
-	cam.cam.still_configuration.controls.ExposureTime = exposure_time
-	cam.cam.still_configuration.controls.AwbEnable = awb_enable
-	cam.cam.still_configuration.controls.AeEnable = ae_enable
-	cam.cam.still_configuration.controls.AnalogueGain = analogue_gain
-	cam.cam.still_configuration.controls.Brightness = brightness
-	cam.cam.still_configuration.controls.Contrast = contrast
-	cam.cam.still_configuration.controls.ColourGains = colour_gains
-	cam.cam.still_configuration.controls.Sharpness = sharpness
-	cam.cam.still_configuration.controls.Saturation = saturation
-	#cam.still_configuration.controls.ExposureValue = exposure_value
-	cam.cam.still_configuration.size = (res_x,res_y)
 
-	exp.logs["cam_settings"] = safepicam2_config(cam.cam.still_configuration)
-	lightmap = {"r": set_red, "g":set_green, "b": set_blue, "w":set_white}
+
+	### Set compression and quality
+	cam.cam.options["quality"] = exp.attribs["quality"]
+	cam.cam.options["compress_level"] = exp.attribs["compress_level"]
+
+	read_config =  safepicam2_config(cam.cam.still_configuration)
+	print(read_config)
+	
+	exp.dealy("Configuration checking delay", 10)
+
 	for channel in lightmap:
 
 		### Set-light color
-		lightmap[channel]()
+		scope.lit.setVs(*lightmap[channel])
 
-		name = f"res_{res[0]}_{res[1]}_{channel}{voltage}".replace(".", "pt")
-		cam.cam.start_and_capture_files(name+"{:d}.png", initial_delay=10, delay=5,
-										num_files=3)
-
-
-		result = {"scope": scopeid, "V": voltage, "channel":channel, "res":res, "magnification": 0.5, 
-				  "name":name, "exp_type":"still_resolution_test_usaftt", 
-				  "target":"usaf_tt", "usaftt_group":None, "usaftt_element":None, "min_res_um":None,
-				  "res": list(cam.cam.still_configuration.size), 
-				  "raw_res": list(cam.cam.still_configuration.sensor["output_size"]),
-				  "replicate":0}
-		from copy import deepcopy
-		for i in range(3):
-			r = deepcopy(result)
-			r["replicate"] = i+1
-			r["acq"] = name+f"{i+1}.png" 
-			exp.logs["results"].append(r)
-
-		#sleeping_t = np.round(np.rand() * 10, 2)
-		sleeping_t = 5
-		print(f"{i:2}. {time.perf_counter()} : Sleeping for: {sleeping_t:2} s.")
-		sleep(sleeping_t)
+		### Capture files
+		name = f"res_{res[0]}_{res[1]}_{channel}".replace(".", "pt")
 		
+
+		for i in range(exp.attribs["itr"]):
+			acq=name+"_cntr_{i}.png"
+			m = ms(rV=lightmap[channel][0], gV=lightmap[channel][1], bV=lightmap[channel][3],
+					res=res, magnification=exp.attribs["magnification"], config_size=list(cam.cam.still_configuration.sensor["output_size"]),
+					itr_no=i, quality=cam.cam.options["quality"], compression=cam.cam.options["compress_level"], acq=acq
+				  )
+
+			exp.dealy("Delay before capture", 5)
+			cam.cam.start_and_capture_file(show_preview=True)
+
+			## Print Measurement Table
+			print(tab)
+
+		
+	### Close camera
 	cam.cam.stop_preview()
 	cam.close()
 
--- transmit/hivemqtt.py
+++ transmit/hivemqtt.py
@@ -1,50 +1,50 @@
-import ssl
-from paho import mqtt
-import paho.mqtt.client as paho
-import paho.mqtt.publish as publish
-import paho.mqtt.subscribe as subscribe
-from paho import mqtt
-from time import sleep
-import ast
+    import ssl
+    from paho import mqtt
+    import paho.mqtt.client as paho
+    import paho.mqtt.publish as publish
+    import paho.mqtt.subscribe as subscribe
+    from paho import mqtt
+    from time import sleep
+    import ast
 
-from rich import print
+    from rich import print
 
-from rich import print
+    from rich import print
 
-class HiveMQTT:
+    class HiveMQTT:
 
-    connected = False
+        connected = False
 
-    def connect_receiver():
-        try:
+        def connect_receiver():
+            try:
+                # use TLS for secure connection with HiveMQ Cloud
+                sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS)
+
+                 # put in your cluster credentials and hostname
+                auth = {'username': "trappyscope", 'password': "cr_in_trappy"}
+                subscribe.callback(HiveMQTT.print_msg, "#", 
+                                hostname="d093a1f309db470597759456d1eeefd4.s1.eu.hivemq.cloud",
+                                port=8883, auth=auth,
+                                tls=sslSettings, protocol=paho.MQTTv31)
+                HiveMQTT.connected = True
+            except:
+                HiveMQTT.connected = False
+
+        def send(topic, message):
+            return
             # use TLS for secure connection with HiveMQ Cloud
             sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS)
-
-             # put in your cluster credentials and hostname
-            auth = {'username': "trappyscope", 'password': "cr_in_trappy"}
-            subscribe.callback(HiveMQTT.print_msg, "#", 
-                            hostname="d093a1f309db470597759456d1eeefd4.s1.eu.hivemq.cloud",
-                            port=8883, auth=auth,
-                            tls=sslSettings, protocol=paho.MQTTv31)
-            HiveMQTT.connected = True
-        except:
-            HiveMQTT.connected = False
-
-    def send(topic, message):
-        return
-        # use TLS for secure connection with HiveMQ Cloud
-        sslSettings = ssl.SSLContext(mqtt.client.ssl.PROTOCOL_TLS)
-    
-        msgs = [{'topic': topic, 'payload': str(message)}]
-
-        # put in your cluster credentials and hostname
-        auth = {'username': "trappyscope", 'password': "cr_in_trappy"}
-        publish.multiple(msgs, hostname="d093a1f309db470597759456d1eeefd4.s1.eu.hivemq.cloud", port=8883, auth=auth,
-                         tls=sslSettings, protocol=paho.MQTTv31)
-        print("HiveMQTT sent >>", message)
-
-
-
         
+            msgs = [{'topic': topic, 'payload': str(message)}]
 
-       
+            # put in your cluster credentials and hostname
+            auth = {'username': "trappyscope", 'password': "cr_in_trappy"}
+            publish.multiple(msgs, hostname="d093a1f309db470597759456d1eeefd4.s1.eu.hivemq.cloud", port=8883, auth=auth,
+                             tls=sslSettings, protocol=paho.MQTTv31)
+            print("HiveMQTT sent >>", message)
+
+
+
+            
+
+           
