fc991b7a98830f1f02233916076179bc1a60fff4
yatharthb97 <yatharth1997@gmail.com>
2024-05-27 15:04:27 +0100

    Minor bug fix

--- scripts/resolutiontests/still_res_test.py
+++ scripts/resolutiontests/still_res_test.py
@@ -86,7 +86,7 @@ m = magms(acq="magnification_std.png", config_size=list(cam.cam.still_configurat
 		  size=cam.cam.still_configuration.size)
 
 exp.user_prompt(None, label="Place the USAF test target and focus at 1X.")
-cam.preview(tsec=100)
+cam.preview(tsec=30)
 
 
 
