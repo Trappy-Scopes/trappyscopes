7cc196c65d1b69ff6023ef0dc9a940310586bd47
yatharthb97 <yatharth1997@gmail.com>
2024-06-12 17:52:22 +0100

    Added new script

--- scripts/resolutiontests/exposure_ag_volt_iteration.py
+++ scripts/resolutiontests/exposure_ag_volt_iteration.py
@@ -170,7 +170,7 @@ for res in exp.attribs["res_set"]:                                  ## 0
 
 							lightmap = get_lightconditions(ch, volt)
 							measurement = ms(channel=ch, voltage=volt, again=again, exposure_time_us=exposure,
-											 rV=lightmap[channel][0], gV=lightmap[channel][1], bV=lightmap[channel][2],
+											 rV=lightmap[ch][0], gV=lightmap[ch][1], bV=lightmap[ch][2],
 											 res=res, magnification=exp.attribs["magnification"], itr_no=i, 
 											 acq=acq, acq_fullpath=os.path.join(exp.exp_dir, acq), *frame_metadata)
 							
