0293824297a388d3617febba38599a199b8c9e33
yatharthb97 <yatharth1997@gmail.com>
2024-07-03 10:11:46 +0100

    sync

--- tsexceptions.py
+++ tsexceptions.py
@@ -1,9 +1,13 @@
 
 
-class InvalidNameException(Exception):
+class TS_InvalidNameException(Exception):
 	"""
 	Raised when an invalid name is passed and normal error handling is not sufficient.
 	Example: during object creation -> use to quit object creation altogether when an
 	invalid name is passed.
 	"""
+	...
+
+
+class TS_DeviceNotRegistered(Exception):
 	...
