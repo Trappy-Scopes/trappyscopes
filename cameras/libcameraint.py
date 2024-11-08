56cd322476bef367c88017ac4d8870565ca8b57a
yatharthb97 <yatharth1997@gmail.com>
2024-11-08 11:29:55 +0000

    libcamera mjpeg mode test

--- cameras/libcameraint.py
+++ cameras/libcameraint.py
@@ -103,8 +103,7 @@ class Camera:
 	def __vid_mjpeg_prev__(self, filename, *args, **kwargs):
 		tsec = kwargs["tsec"]
 
-		cmd_list = f"libcamera-vid -t {tsec*1000} -f -o {filename} --codec {mjpeg} --width 2028 --height 2028 --denoise off --awbgains 0,0 \
-					 --analoggain 1 --framerate {kwargs["fps"]} --shutter {kwargs["exposure_ms"]} -q 95"
+		cmd_list = f"libcamera-vid -t {tsec*1000} -f -o {filename} --codec {mjpeg} --width 2028 --height 2028 --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs["fps"]} --shutter {kwargs["exposure_ms"]} -q 95"
 		return self.__process__(cmd_list)
 
 	def is_open(self):
