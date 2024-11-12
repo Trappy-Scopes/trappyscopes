27cead47fa5936d8147b6d412a4fd6cc143230da
yatharthb97 <yatharth1997@gmail.com>
2024-11-12 15:25:36 +0000

    Added new scripts

---
+++ scripts/longterm/mjpeg_sampling_tandh.py
@@ -0,0 +1,43 @@
+from datetime import timedelta
+import datetime
+import time
+from rich import print
+
+def create_exp():
+	global exp
+	dt = str(datetime.date.today()).replace("-", "_")
+	t = time.localtime(time.time())
+	time_str = f"{t.tm_hour}hh_{t.tm_min}mm"
+	exp = Experiment(f"{scopeid}_{dt}_{time_str}_sampled_longterm_traj", append_eid=True)
+
+	exp.new_measurementstream("tandh", measurements=["temp", "humidity"])
+	exp.new_measurementstream("acq", monitors=["acq"])
+
+print("Use create_exp() to open a new experiment. Use findexp() to open an old one.")
+print("Use start_acq() to start acquiring.")
+
+
+
+capture = lambda: scope.cam.capture("vid_mjpeg_prev", f"{str(datetime.datetime.now()).split(".")[0].replace(" ", "_")}",  tsec=30, fps=30, exposure_ms=25)
+def start_acq():
+
+	global exp, scope, capture
+	
+
+	## Read tandh
+	tandh = exp.streams["tandh"]
+	record_sensor = lambda: tandh(**scope.tandh.read())
+	record_sensor()
+	exp.schedule.every(5).minutes.until(timedelta(hours=24)).do(record_sensor)
+	exp.note("Tandh sensors set to record every 5 mins for the next 24 hours, starting now.")
+
+
+	## Set lights and capture
+
+	scope.lit.setVs(3,3,3)
+	capture()
+	exp.schedule.every(1).hours.until(timedelta(hours=24)).do(capture)
+	exp.note("Set to capture 30 seconds, every 1 hour for the next 24 hours, starting now.")
+	
+
+
