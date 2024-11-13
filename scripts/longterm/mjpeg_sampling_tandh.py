e088a6befeabd82144639837217ff220150916fc
yatharthb97 <yatharth1997@gmail.com>
2024-11-13 12:38:56 +0000

    Made changes to a script

--- scripts/longterm/mjpeg_sampling_tandh.py
+++ scripts/longterm/mjpeg_sampling_tandh.py
@@ -18,7 +18,7 @@ print("Use start_acq() to start acquiring.")
 
 
 
-capture = lambda: scope.cam.capture("vid_mjpeg_prev", f"{str(datetime.datetime.now()).split(".")[0].replace(" ", "_")}",  tsec=30, fps=30, exposure_ms=(1/30)*1000*0.5)
+capture = lambda: scope.cam.capture("vid_mjpeg_prev", f"{str(datetime.datetime.now()).split(".")[0].replace(" ", "__").replace(":", "_").replace("-", "_")}",  tsec=30, fps=30, exposure_ms=(1/30)*1000*0.5)
 def start_acq():
 
 	global exp, scope, capture
