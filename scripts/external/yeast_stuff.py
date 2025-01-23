535e408ca28375bd7dc8c24320f1ae0510ed24e2
yatharthb97 <yatharth1997@gmail.com>
2025-01-23 14:02:40 +0000

    Libcamera - img mode bugs corrected

--- scripts/external/yeast_stuff.py
+++ scripts/external/yeast_stuff.py
@@ -26,7 +26,7 @@ def yeast_capture_AG(sample, tsec=10, force=False):
 	dt = str(datetime.date.today()).replace("-", "_")
 	t = time.localtime(time.time())
 	time_str = f"{t.tm_hour}hh_{t.tm_min}mm"
-	filename = f"{sample}_{dt}_{time_str}.png"
+	filename = f"{sample}_AG_{dt}_{time_str}.png"
 
 	if os.path.isfile(filename) and not force:
 		print(f"[red][bold] ERROR : [red] Filename already exists: {filename}")
@@ -57,13 +57,13 @@ def yeast_capture_MV(sample, tsec=10, force=False):
 		exp = Experiment(exp_name)
 
 	## Login
-	if User.info["user"] != "AG":
-		User.login("AG")
+	if User.info["user"] != "MV":
+		User.login("MV")
 
 	dt = str(datetime.date.today()).replace("-", "_")
 	t = time.localtime(time.time())
 	time_str = f"{t.tm_hour}hh_{t.tm_min}mm"
-	filename = f"{sample}_{dt}_{time_str}.png"
+	filename = f"{sample}_MV_{dt}_{time_str}.png"
 
 	if os.path.isfile(filename) and not force:
 		print(f"[red][bold] ERROR : [red] Filename already exists: {filename}")
