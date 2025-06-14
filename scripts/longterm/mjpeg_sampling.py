8f9443858a5c854a863d1c35578805d8624b6044
yatharthb97 <yatharth1997@gmail.com>
2025-06-14 17:50:53 +0100

    Small bug

--- scripts/longterm/mjpeg_sampling.py
+++ scripts/longterm/mjpeg_sampling.py
@@ -47,7 +47,7 @@ def capture():
 	exp = Experiment.current
 	split = len(exp.mstreams["acq"].readings)
 	filename=exp.newfile(f'{str(datetime.datetime.now()).split(".")[0].replace(" ", "__").replace(":", "_").replace("-", "_")}__split_{split}.mjpeg', abspath=False)
-	acq = exp.mstreams["acq"](filename)
+	acq = exp.mstreams["acq"](filename=filename)
 	scope.cam.capture(exp.params["camera_action"],
 	                  filename,
 	                  tsec=exp.params["capture_time_sec"],
