4974256e73a36c435dc38fffd3c3089e7c052154
yatharthb97 <yatharth1997@gmail.com>
2025-07-15 14:51:54 +0100

    Modifying picam2 library

--- detectors/cameras/rpi_hq_picam2.py
+++ detectors/cameras/rpi_hq_picam2.py
@@ -95,7 +95,7 @@ class Camera(AbstractCamera):
         log.info("TS::Camera::PiCamera2 Camera was opened.")
         self.cam = Picamera2()
         self.video_config = self.cam.create_video_configuration(buffer_count=6, 
-            main={"size":(self.config["res"][0], self.config["res"][1]), 'format': 'YUV420'},
+            main={"size":(self.config["res"][0], self.config["res"][1])},
             #controls=self.controls, 
             encode="main", display="main")
         self.cam.configure(self.video_config)
