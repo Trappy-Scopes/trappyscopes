cf7d8601e9d62906ee339ab7a0b10d6299644413
yatharthb97 <yatharth1997@gmail.com>
2025-07-15 18:33:49 +0100

    Modifying picam2 library

--- detectors/cameras/rpi_hq_picam2.py
+++ detectors/cameras/rpi_hq_picam2.py
@@ -57,8 +57,8 @@ class JpegEncoderGrayRedCh(JpegEncoder):
                 #bgr_frame = 
                 #r_frame = bgr_frame
                 #r_frame = r_frame
-                print(self.colour_space)
-                return simplejpeg.encode_jpeg(m.array.reshape(height, width, 3)[:, :, 2].reshape((height, width, 1), order='C', dtype=np.uint8),
+                print(m.array.reshape(height, width, 3)[:, :, 2].reshape((height, width, 1), order='C').shape)
+                return simplejpeg.encode_jpeg(m.array,
                                           quality=self.q, colorspace="GRAY", colorsubsampling='Gray')
 
 
