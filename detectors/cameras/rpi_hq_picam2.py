8b9ca7c56400ae321931b75f2eb840fdbf66d55c
yatharthb97 <yatharth1997@gmail.com>
2025-07-15 15:04:52 +0100

    Modifying picam2 library

--- detectors/cameras/rpi_hq_picam2.py
+++ detectors/cameras/rpi_hq_picam2.py
@@ -52,7 +52,7 @@ class Camera(AbstractCamera):
 
         self.config  = {"res":res, "quality":quality, "fps":fps, "compression":compression, "exposure_ms":18}
         self.controls = {"ExposureTime": self.config["exposure_ms"]*1000, "AnalogueGain": 1.0, "AwbEnable": False, "AeEnable":False, 
-                         "ColourGains":(1.0,1.0), "Contrast":2.0, 
+                         "ColourGains":(0.0,0.0), "Contrast":2.0, 
                         # "NoiseReductionMode":controls.draft.NoiseReductionModeEnum.Off, 
                         # 'FrameDurationLimits':(1e6/self.config["fps"], 1e6/self.config["fps"])
                         }
