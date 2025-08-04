8f7cae898a9949eec87c015da347dd18571cd49f
yatharthb97 <yatharth1997@gmail.com>
2025-08-04 17:14:00 +0100

    Long term light measurements.

--- scripts/calibration/light_stability.py
+++ scripts/calibration/light_stability.py
@@ -1,50 +1,53 @@
 from datetime import timedelta
-import datetime
-import time
+
+
+
 from rich import print
 import ast
 from rich.panel import Panel
 from rich.pretty import Pretty
 
+from hive.assembly import ScopeAssembly
 from expframework.experiment import Experiment 
 
 
-## Create a calibration context
-dt = str(datetime.date.today()).replace("-", "_")
-t = time.localtime(time.time())
-time_str = f"{t.tm_hour}hh_{t.tm_min}mm"
-exp = Experiment(f"{scopeid}_{dt}_{time_str}_light_calibration", append_eid=True)
-tandh = exp.new_measurementstream("photon_counts", measurements=["light", "photon_count"])
 
-## Define a light condition
-exp.attribs["light"] = (2,0,0)
-exp.attribs["sample_period_s"] = 60
-exp.attribs["total_time_hours"] = 24
-scope.beacon.blink()
+def create_exp():
+	global exp, scope
+	exp = Experiment.Construct(["light", "longterm", "stability"])
+	tandh = exp.new_measurementstream("lux_sensor_count", measurements=["light", "counts", "temp"])
+
+	## Define a light condition
+	exp.attribs["light"] = (2,0,0)
+	exp.attribs["sample_period_s"] = 60
+	exp.attribs["total_time_hours"] = 24
+	scope.beacon.blink()
 
 def start():
-
 	global exp, scope, capture
 	scope.beacon.on()
 
 
 	## Read tandh
-	photon_counts = exp.mstreams["photon_counts"]
+	photon_counts = exp.mstreams["lux_sensor_count"]
 	scope.lit.setVs(*exp.attribs["light"])
 	def record_sensor():
+		global exp, scope
 		try:
 			value = scope.light_sensor.read()
 		except:
 			print("[red]:Light sensor reading failed![default]")
 			value =  {"light": None, "photon_count":None}
-		r = photon_counts(light=exp.attribs["light"], **value)
+		r = photon_counts(light=exp.attribs["light"], temp=scope.tandh.read()["temp"], **value)
 		r.panel()	
 	
+	## 0th measurement
 	record_sensor()
+	
 	exp.schedule.every(exp.attribs["sample_period_s"]).seconds.until(timedelta(hours=exp.attribs["total_time_hours"])).do(record_sensor)
 	print(f'Light sensor value set to record every {exp.attribs["sample_period_s"]} seconds for the next {exp.attribs["total_time_hours"]} hours, starting now.')
 	exp.logs.update(scope.get_config())
-	exp.__save__()
+
 
 def stop():
 	global exp, scope, capture
@@ -52,4 +55,3 @@ def stop():
 	exp.sync_dir()
 	exp.close()
 
-	
