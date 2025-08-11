f535f84a732adb0dad740afe3668f8e92aac8d1a
yatharthb97 <yatharth1997@gmail.com>
2025-08-11 18:01:43 +0100

    Preview type changed to DRM

--- detectors/cameras/rpi_hq_picam2.py
+++ detectors/cameras/rpi_hq_picam2.py
@@ -140,8 +140,8 @@ class Camera(AbstractCamera):
         atexit.unregister(self.close)
 
 
-    def preview(self, tsec=10, size=(1520, 1520)):
-        self.cam.start_preview(self.preview_type, height=size[0], width=size[1])
+    def preview(self, tsec=10, size=(1520, 1520), xy=[0,0]):
+        self.cam.start_preview(self.preview_type, height=size[0], width=size[1], x=xy[0], y=xy[1])
         self.cam.start()
         precise_sleep(tsec)
         self.cam.stop()
