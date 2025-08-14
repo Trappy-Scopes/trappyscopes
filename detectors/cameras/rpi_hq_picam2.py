eaa894d2ea13136277505a97c76324d0fb89c6e9
yatharthb97 <yatharth1997@gmail.com>
2025-08-14 16:47:38 +0100

    Updated scripts

--- detectors/cameras/rpi_hq_picam2.py
+++ detectors/cameras/rpi_hq_picam2.py
@@ -407,6 +407,7 @@ class Camera(AbstractCamera):
         self.cam.start_recording(encoder, output)
         
         try:
+            print("Beginning iterations...")
             for file_no in range(no_iterations):
                 
                 ## Geenrate splitname
