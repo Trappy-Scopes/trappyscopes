274f93ae857181bfa402d696d8ee76c1d68956ad
yatharthb97 <yatharth1997@gmail.com>
2026-04-24 10:01:11 +0100

    Added multicamera support and multiplex option for libcamera or rpicam utility selection to libcamera interface

--- detectors/cameras/libcamera.py
+++ detectors/cameras/libcamera.py
@@ -6,7 +6,9 @@ import os
 from detectors.cameras.abstractcamera import Camera as AbstractCamera
 
 class Camera():
-    def __init__(self):
+    def __init__(self, *args, program_name="libcamera", **kwargs):
+        """Pass camera keyword for multiple cameras.
+           program_name: defaults to `libcamera`. For newer installs, use `rpicam`."""
         super().__init__()
         self.process = None
         self.status  = "standby"
@@ -17,7 +19,15 @@ class Camera():
                         "vid_mjpeg": self.__vid_mjpeg__,
                         "vid_mjpeg_tpts": self.__vid_mjpeg_tpts__,
                         "prev_formatted": self.__preview_formatted__}
-        self.config={"kind":"camera-libcamera", "res": (2028, 2028), "fps":20}
+        self.config={"kind":"camera-libcamera", "res": (2028, 2028), "fps":20, "program_name": program_name}
+        if "camera" in kwargs:
+            self.config["camera"] = kwargs["camera"]
+
+    def __add_optional_args__(self, cmd):
+        """Function to handle case-by-case condtions"""
+        if "camera" in self.config: ## Used for a multi-camera setup.
+            cmd =  f"{cmd} ---camera {self.config['camera']}"
+        return cmd
 
     def __process__(self, cmd):
         self.process = Popen(cmd, stdout=sys.stdout, stderr=sys.stderr, shell=True,\
@@ -32,8 +42,8 @@ class Camera():
 
 
     def __preview__(self, tsec=10):
-        cmd_list = f"libcamera-vid -t {tsec*1000} -f"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -f"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __preview_formatted__(self, filename, *args, **kwargs):
         """
@@ -45,23 +55,23 @@ class Camera():
         self.config["fps"] = kwargs["fps"]
         fps = self.config["fps"]
         self.config["exposure_ms"] = kwargs["exposure_ms"]
-        cmd_list = f"libcamera-vid -t {tsec*1000} -f --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} --contrast 2 --sharpness 1 -q {kwargs['quality']}  --buffer-count 6"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -f --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} --contrast 2 --sharpness 1 -q {kwargs['quality']}  --buffer-count 6"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __image__(self, filename, *args, tsec=5, **kwargs):
         print(f"Preview will last {tsec} seconds!")
         res = self.config["res"]
         self.config["exposure_ms"] = kwargs["exposure_ms"]
-        cmd_list = f"libcamera-still -t {tsec*1000} -o {filename} --width {res[0]} --height {res[1]}  --denoise off --awbgains 0,0 --analoggain 1  --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --buffer-count 6"
+        cmd_list = f"{self.config['program_name']}-still -t {tsec*1000} -o {filename} --width {res[0]} --height {res[1]}  --denoise off --awbgains 0,0 --analoggain 1  --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --buffer-count 6"
         if filename.endswith(".png"):
             cmd_list += " -e png"
-        return self.__process__(cmd_list)
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __video__(self, filename, *args, **kwargs):
         res = self.config["res"]    
         tsec = kwargs["tsec"]
-        cmd_list = f"libcamera-vid -t {tsec*1000} -f -o {filename}  --buffer-count 6"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -f -o {filename}  --buffer-count 6"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __vid_mjpeg_prev__(self, filename, *args, **kwargs):
         tsec = kwargs["tsec"]
@@ -70,8 +80,8 @@ class Camera():
         self.config["fps"] = kwargs["fps"]
         fps = self.config["fps"]
         self.config["exposure_ms"] = kwargs["exposure_ms"]
-        cmd_list = f"libcamera-vid -t {tsec*1000} -f -o {filename} --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --buffer-count 6"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -f -o {filename} --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --buffer-count 6"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __vid_mjpeg__(self, filename, *args, **kwargs):
         tsec = kwargs["tsec"]
@@ -80,8 +90,8 @@ class Camera():
         self.config["fps"] = kwargs["fps"]
         fps = self.config["fps"]
         self.config["exposure_ms"] = kwargs["exposure_ms"]
-        cmd_list = f"libcamera-vid -t {tsec*1000} -o {filename} --nopreview --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']}  --buffer-count 6"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -o {filename} --nopreview --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']}  --buffer-count 6"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
     def __vid_mjpeg_tpts__(self, filename, *args, **kwargs):
         tsec = kwargs["tsec"]
@@ -91,8 +101,8 @@ class Camera():
         self.config["fps"] = kwargs["fps"]
         fps = self.config["fps"]
         self.config["exposure_ms"] = kwargs["exposure_ms"]
-        cmd_list = f"libcamera-vid -t {tsec*1000} -o {filename} --nopreview --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --save-pts {ptsfname} --contrast 2 --sharpness 1 --buffer-count 6"
-        return self.__process__(cmd_list)
+        cmd_list = f"{self.config['program_name']}-vid -t {tsec*1000} -o {filename} --nopreview --codec mjpeg --width {res[0]} --height {res[1]} --denoise off --awbgains 0,0 --analoggain 1 --framerate {kwargs['fps']} --shutter {kwargs['exposure_ms']*1000} -q {kwargs['quality']} --save-pts {ptsfname} --contrast 2 --sharpness 1 --buffer-count 6"
+        return self.__process__(self.__add_optional_args__(cmd_list))
 
 
 
